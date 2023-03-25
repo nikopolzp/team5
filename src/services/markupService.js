65052a6222521cdbcf5227646af7dde7d343f5f3
Kateryna Verveda <katushka211@gmail.com>
2023-03-25 15:01:58 +0100

    add task-02

--- src/services/markupService.js
+++ src/services/markupService.js
@@ -3,3 +3,7 @@ export function createMarkupProductsList(products) {
     .map((product) => `<li><h2>${product.title}</h2></li>`)
     .join("");
 }
+
+export function createProductById(product) {
+  return `<h2>${product.title}</h2><p>${product.price}</p>`;
+}
